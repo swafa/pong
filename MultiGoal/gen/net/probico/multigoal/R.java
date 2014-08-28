d97bbc17b333022b20486ecb1e33b5eef5b2a79a
Samir Wafa <samir.wafa@media-it.de>
2014-08-28 17:30:41 +0200

    Usage of PongUtils library

--- MultiGoal/gen/net/probico/multigoal/R.java
+++ MultiGoal/gen/net/probico/multigoal/R.java
@@ -214,14 +214,13 @@ containing a value of this type.
         public static final int common_signin_btn_text_pressed_light=0x7f020019;
         public static final int gamescreen_background=0x7f02001a;
         public static final int homescreen_background=0x7f02001b;
-        public static final int ic_launcher=0x7f02001c;
-        public static final int ic_plusone_medium_off_client=0x7f02001d;
-        public static final int ic_plusone_small_off_client=0x7f02001e;
-        public static final int ic_plusone_standard_off_client=0x7f02001f;
-        public static final int ic_plusone_tall_off_client=0x7f020020;
-        public static final int launching_icon=0x7f020021;
-        public static final int logo_multigoal=0x7f020022;
-        public static final int paddle=0x7f020023;
+        public static final int ic_plusone_medium_off_client=0x7f02001c;
+        public static final int ic_plusone_small_off_client=0x7f02001d;
+        public static final int ic_plusone_standard_off_client=0x7f02001e;
+        public static final int ic_plusone_tall_off_client=0x7f02001f;
+        public static final int launching_icon=0x7f020020;
+        public static final int logo_multigoal=0x7f020021;
+        public static final int paddle=0x7f020022;
     }
     public static final class id {
         public static final int easy_button=0x7f060008;
--- MultiGoal/src/net/probico/multigoal/GameplayFragment.java
+++ MultiGoal/src/net/probico/multigoal/GameplayFragment.java
@@ -1,81 +1,33 @@
-/*
- * Copyright (C) 2013 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.probico.multigoal;
 
-import net.probico.multigoal.R;
-import net.probico.multigoal.MainActivity.GameMode;
-import android.graphics.Color;
+import net.probico.pong.PongGameplayFragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.RelativeLayout.LayoutParams;
-import android.widget.TextView;
-
-import com.google.android.gms.games.Games;
 
 /**
- * Fragment for the gameplay portion of the game. It shows the keypad where the
- * user can request their score.
+ * Fragment for the gameplay portion of the game.
  * 
- * @author Bruno Oliveira (Google)
- * 
+ * @author samir
  */
-public class GameplayFragment extends Fragment {
-	// int mRequestedScore = 5000;
-	//
-	// static int[] MY_BUTTONS = {
-	// R.id.digit_button_0, R.id.digit_button_1, R.id.digit_button_2,
-	// R.id.digit_button_3, R.id.digit_button_4, R.id.digit_button_5,
-	// R.id.digit_button_6, R.id.digit_button_7, R.id.digit_button_8,
-	// R.id.digit_button_9, R.id.digit_button_clear, R.id.ok_score_button
-	// };
-	//
-	// public interface Listener {
-	// public void onEnteredScore(int score);
-	// }
-	//
-	// Listener mListener = null;
+public class GameplayFragment extends PongGameplayFragment {
 
 	private static final String MAIN_ACTIVITY_KEY = "main_activity_key";
-	private MainActivity activity;
 
-	TextView countDownTxtView;
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return super.onCreateView(inflater, container, savedInstanceState);
+	}
 
-	RelativeLayout topLayout;
-	RelativeLayout bottomLayout;
+	@Override
+	protected int getLayoutResourceId() {
+		return R.layout.fragment_gameplay;
+	}
 
-	TextView player1ScoreTxtView;
-	TextView player2ScoreTxtView;
-
-	TextView youWinTxtView;
-	TextView youLoseTxtView;
-
-	FrameLayout gameLayout;
-
-	// public GameplayFragment(MainActivity activity){
-	// this.activity = activity;
-	// }
-
-	public static GameplayFragment newInstance(MainActivity mainActivity) {
+	public static GameplayFragment newInstance(
+			MainActivity mainActivity) {
 		GameplayFragment fragment = new GameplayFragment();
 		Bundle bundle = new Bundle();
 		bundle.putSerializable(MAIN_ACTIVITY_KEY, mainActivity);
@@ -86,328 +38,8 @@ public class GameplayFragment extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_gameplay, container, false);
-		// for (int i : MY_BUTTONS) {
-		// ((Button) v.findViewById(i)).setOnClickListener(this);
-		// }
-
-		gameLayout = (FrameLayout) v.findViewById(R.id.screen_gameplay);
-		activity = (MainActivity) getArguments().getSerializable(
-				MAIN_ACTIVITY_KEY);
-
-		inflateGameLayout();
-		return v;
-	}
-
-	public void inflateGameLayout() {
-
-		String displayName;
-
-		if (activity.getApiClientInstance().isConnected()
-				&& Games.Players.getCurrentPlayer(activity
-						.getApiClientInstance()) != null) {
-			displayName = Games.Players.getCurrentPlayer(
-					activity.getApiClientInstance()).getDisplayName();
-		} else {
-			displayName = "You";
-		}
-
-		activity.getGlSurfaceView().getRenderer().setFirstRender(true);
-
-		if (activity.getGlSurfaceView().getParent() != null) {
-			((FrameLayout) activity.getGlSurfaceView().getParent())
-					.removeAllViews();
-		}
-
-		gameLayout.addView(activity.getGlSurfaceView());
-
-		LinearLayout ll = new LinearLayout(activity);
-		ll.setOrientation(LinearLayout.VERTICAL);
-
-		topLayout = new RelativeLayout(activity);
-		LinearLayout.LayoutParams relativeLayoutParameters = new LinearLayout.LayoutParams(
-				LinearLayout.LayoutParams.MATCH_PARENT,
-				LinearLayout.LayoutParams.MATCH_PARENT, 1);
-		topLayout.setLayoutParams(relativeLayoutParameters);
-
-		player2ScoreTxtView = new TextView(activity);
-		player2ScoreTxtView.setTextColor(Color.WHITE);
-		player2ScoreTxtView.setTextAppearance(activity, R.style.LargeTextStyle);
-		RelativeLayout.LayoutParams scoreTopLayoutParamaters = new RelativeLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		scoreTopLayoutParamaters.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-		player2ScoreTxtView.setLayoutParams(scoreTopLayoutParamaters);
-		player2ScoreTxtView.setText(activity.getPlayer2Score() + "");
-		topLayout.addView(player2ScoreTxtView);
-		ll.addView(topLayout);
-
-		bottomLayout = new RelativeLayout(activity);
-		bottomLayout.setLayoutParams(relativeLayoutParameters);
-
-		if (activity.getGameMode().equals(GameMode.SINGLE_PLAYER)
-				|| activity.getGameMode().equals(GameMode.TWO_PLAYERS_ONLINE)) {
-
-			RelativeLayout.LayoutParams currentPlayerNameLayoutParams = new RelativeLayout.LayoutParams(
-					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-
-			TextView currentPlayerNameTxtView = new TextView(activity);
-			currentPlayerNameTxtView.setTextColor(Color.WHITE);
-			currentPlayerNameTxtView.setTextAppearance(activity,
-					R.style.SmallTextStyle);
-			currentPlayerNameLayoutParams
-					.addRule(RelativeLayout.CENTER_VERTICAL);
-			// vlp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-			currentPlayerNameTxtView
-					.setLayoutParams(currentPlayerNameLayoutParams);
-
-			currentPlayerNameTxtView.setText(displayName);
-			// tv.setBackgroundColor(0x4060ff70);
-
-			if ((activity.getGameMode().equals(GameMode.SINGLE_PLAYER) || (activity
-					.getGameMode().equals(GameMode.TWO_PLAYERS_ONLINE) && !activity
-					.isCurrentParticipantInvitee()))) {
-				bottomLayout.addView(currentPlayerNameTxtView);
-			} else if (activity.getGameMode().equals(
-					GameMode.TWO_PLAYERS_ONLINE)
-					&& activity.isCurrentParticipantInvitee()) {
-				topLayout.addView(currentPlayerNameTxtView);
-			}
-
-		}
-
-		RelativeLayout.LayoutParams scoreBottomLayoutParams = new RelativeLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-		player1ScoreTxtView = new TextView(activity);
-		player1ScoreTxtView.setTextColor(Color.WHITE);
-		player1ScoreTxtView.setTextAppearance(activity, R.style.LargeTextStyle);
-		scoreBottomLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-		// vlp.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-		player1ScoreTxtView.setLayoutParams(scoreBottomLayoutParams);
-		player1ScoreTxtView.setText(activity.getPlayer1Score() + "");
-		// tv.setBackgroundColor(0x4060ff70);
-		bottomLayout.addView(player1ScoreTxtView);
-
-		RelativeLayout.LayoutParams resultMessageLayoutParameters = new RelativeLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-
-		youWinTxtView = new TextView(activity);
-		youWinTxtView.setText("YOU WIN!");
-		youWinTxtView.setTextColor(Color.BLUE);
-		youWinTxtView.setTextAppearance(activity, R.style.XLargeTextStyle);
-		resultMessageLayoutParameters.addRule(RelativeLayout.CENTER_IN_PARENT);
-
-		youWinTxtView.setLayoutParams(resultMessageLayoutParameters);
-
-		youLoseTxtView = new TextView(activity);
-		youLoseTxtView.setText("YOU LOSE!");
-		youLoseTxtView.setTextColor(Color.BLUE);
-		youLoseTxtView.setTextAppearance(activity, R.style.XLargeTextStyle);
-		resultMessageLayoutParameters.addRule(RelativeLayout.CENTER_IN_PARENT);
-
-		youLoseTxtView.setLayoutParams(resultMessageLayoutParameters);
-
-		ll.addView(bottomLayout);
-
-		gameLayout.addView(ll);
-
-		// TextView countDownTxtView = new TextView(activity);
-		// countDownTxtView.setText("1 sec");
-		// countDownTxtView.setTextColor(Color.BLUE);
-		// countDownTxtView.setTextAppearance(activity,
-		// R.style.XLargeTextStyle);
-		// resultMessageLayoutParameters.addRule(RelativeLayout.CENTER_IN_PARENT);
-		// ll.addView(countDownTxtView);
-		//
-		// try {
-		// Thread.sleep(3000);
-		// } catch (InterruptedException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// countDownTxtView.setText("2 sec");
-
-	}
-
-	// public void setListener(Listener l) {
-	// mListener = l;
-	// }
-
-	@Override
-	public void onStart() {
-		super.onStart();
-		// updateUi();
-	}
-
-	void updateUi() {
-		// if (getActivity() == null) return;
-		// TextView scoreInput = ((TextView)
-		// getActivity().findViewById(R.id.score_input));
-		// if (scoreInput != null) scoreInput.setText(String.format("%04d",
-		// mRequestedScore));
-	}
-
-	// @Override
-	// public void onClick(View view) {
-	// switch (view.getId()) {
-	// case R.id.digit_button_clear:
-	// mRequestedScore = 0;
-	// updateUi();
-	// break;
-	// case R.id.digit_button_0:
-	// case R.id.digit_button_1:
-	// case R.id.digit_button_2:
-	// case R.id.digit_button_3:
-	// case R.id.digit_button_4:
-	// case R.id.digit_button_5:
-	// case R.id.digit_button_6:
-	// case R.id.digit_button_7:
-	// case R.id.digit_button_8:
-	// case R.id.digit_button_9:
-	// int x = Integer.parseInt(((Button)view).getText().toString().trim());
-	// mRequestedScore = (mRequestedScore * 10 + x) % 10000;
-	// updateUi();
-	// break;
-	// case R.id.ok_score_button:
-	// mListener.onEnteredScore(mRequestedScore);
-	// break;
-	// }
-	// }
-
-	public void updateScoresUi() {
-		activity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-
-				player1ScoreTxtView.setText(activity.getPlayer1Score() + "");
-				player2ScoreTxtView.setText(activity.getPlayer2Score() + "");
-
-			}
-
-		});
-
-	}
-
-	public void updateUiPlayer1Win() {
-
-		activity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				if (activity.getGameMode().equals(GameMode.SINGLE_PLAYER)) {
-					bottomLayout.addView(youWinTxtView);
-				} else if (activity.getGameMode().equals(GameMode.TWO_PLAYERS)) {
-					bottomLayout.addView(youWinTxtView);
-					topLayout.addView(youLoseTxtView);
-				} else if (activity.getGameMode().equals(
-						GameMode.TWO_PLAYERS_ONLINE)) {
-
-					if (!activity.isCurrentParticipantInvitee()) {
-						topLayout.removeView(youWinTxtView);
-						bottomLayout.removeView(youWinTxtView);
-
-						bottomLayout.addView(youWinTxtView);
-					}
-
-					else {
-						bottomLayout.removeView(youLoseTxtView);
-						topLayout.removeView(youLoseTxtView);
-
-						topLayout.addView(youLoseTxtView);
-					}
-				}
-
-			}
-
-		});
-
-	}
-
-	public void updateUiPlayer2Win() {
-
-		activity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-
-				if (activity.getGameMode().equals(GameMode.SINGLE_PLAYER)) {
-					bottomLayout.addView(youLoseTxtView);
-				} else if (activity.getGameMode().equals(GameMode.TWO_PLAYERS)) {
-					bottomLayout.addView(youLoseTxtView);
-					topLayout.addView(youWinTxtView);
-				} else if (activity.getGameMode().equals(
-						GameMode.TWO_PLAYERS_ONLINE)) {
-
-					if (!activity.isCurrentParticipantInvitee()) {
-						topLayout.removeView(youLoseTxtView);
-						bottomLayout.removeView(youLoseTxtView);
-						bottomLayout.addView(youLoseTxtView);
-					}
-
-					else {
-						bottomLayout.removeView(youWinTxtView);
-						topLayout.removeView(youWinTxtView);
-						topLayout.addView(youWinTxtView);
-					}
-				}
-
-			}
-
-		});
-
-	}
-
-	public void showCountDown() {
-		final RelativeLayout.LayoutParams layoutParameters = new RelativeLayout.LayoutParams(
-				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-
-		layoutParameters.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-		layoutParameters.addRule(RelativeLayout.CENTER_HORIZONTAL);
-
-		countDownTxtView = new TextView(activity);
-
-		countDownTxtView.setText("3");
-
-		countDownTxtView.setTextColor(getResources().getColor(R.color.white));
-		countDownTxtView.setTextAppearance(activity, R.style.XLargeTextStyle);
-		countDownTxtView.setLayoutParams(layoutParameters);
-
-		activity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				topLayout.addView(countDownTxtView);
-			}
-
-		});
-
-		 try {
-		 for (int i = 3; i > 0; i--) {
-		 setTextFieldText(countDownTxtView, i + "");
-		 Thread.sleep(1000);
-		 }
-		
-		 } catch (InterruptedException e) {
-		 // TODO Auto-generated catch block
-		 e.printStackTrace();
-		 }
-
-		activity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				topLayout.removeView(countDownTxtView);
-			}
-
-		});
-	}
-
-	private void setTextFieldText(final TextView txtView, final String text) {
-		activity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				txtView.setText(text);
-			}
-
-		});
-
+	public int getGameLayoutResourceId() {
+		return R.id.screen_gameplay;
 	}
 
 }
